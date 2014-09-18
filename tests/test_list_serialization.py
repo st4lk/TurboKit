f47fcd4b7c7996b68a0f3764c913062cb1155fb6
st4lk <myhappydo@gmail.com>
2014-09-18 18:37:00 +0400

    test all, filter prefetched list of child related models

--- tests/test_list_serialization.py
+++ tests/test_list_serialization.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from bson.objectid import ObjectId
 from tornado.testing import gen_test
 from tornado import gen
 from example_app.models import RecordSeries, SimpleModel, Record, Event, User
@@ -11,7 +10,7 @@ class TestSerializationReferenceList(BaseSerializationTest):
 
     @gen_test
     def test_save_get_ids_only(self):
-        rs, simplies, records, main_event = yield self._create_recordseires(commit=False)
+        rs, simplies, records, main_event = yield self._create_recordseries(commit=False)
         yield rs.save(self.db)
         rs_from_db = yield self.model.objects.set_db(self.db).get({"id": rs.pk})
         self.assertRecordSeriesEquals(rs_from_db, simplies, records, main_event)
@@ -20,7 +19,7 @@ class TestSerializationReferenceList(BaseSerializationTest):
     def test_all_ids_only(self):
         data_list = []
         for i in range(3):
-            data = yield self._create_recordseires()
+            data = yield self._create_recordseries()
             data_list.append(data)
         rss_from_db = yield self.model.objects.set_db(self.db).all()
         self.assertEqual(len(rss_from_db), 3)
@@ -34,7 +33,7 @@ class TestSerializationReferenceList(BaseSerializationTest):
     def test_filter_ids_only(self):
         data_list = []
         for i in range(4):
-            data = yield self._create_recordseires()
+            data = yield self._create_recordseries()
             data_list.append(data)
         ids = [data_list[1][0].pk, data_list[2][0].pk]
         rss_from_db = yield self.model.objects.set_db(self.db)\
@@ -48,7 +47,7 @@ class TestSerializationReferenceList(BaseSerializationTest):
 
     @gen_test
     def test_prefetch_related_get_list_root_fields(self):
-        rs, simplies, records, main_event = yield self._create_recordseires()
+        rs, simplies, records, main_event = yield self._create_recordseries()
         rs_from_db = yield self.model.objects.set_db(self.db)\
             .prefetch_related('simplies').get({"id": rs.pk})
         self.assertRecordSeriesEquals(rs_from_db, simplies, records,
@@ -58,7 +57,7 @@ class TestSerializationReferenceList(BaseSerializationTest):
     def test_prefetch_related_all_list_root_fields(self):
         data_list = []
         for i in range(3):
-            data = yield self._create_recordseires()
+            data = yield self._create_recordseries()
             data_list.append(data)
         rss_from_db = yield self.model.objects.set_db(self.db)\
             .prefetch_related('simplies').all()
@@ -73,7 +72,7 @@ class TestSerializationReferenceList(BaseSerializationTest):
     def test_prefetch_related_filter_list_root_fields(self):
         data_list = []
         for i in range(4):
-            data = yield self._create_recordseires()
+            data = yield self._create_recordseries()
             data_list.append(data)
         ids = [data_list[1][0].pk, data_list[2][0].pk]
         rss_from_db = yield self.model.objects.set_db(self.db)\
@@ -88,7 +87,7 @@ class TestSerializationReferenceList(BaseSerializationTest):
 
     @gen_test
     def test_prefetch_related_get_list_child_fields(self):
-        rs, simplies, records, main_event = yield self._create_recordseires()
+        rs, simplies, records, main_event = yield self._create_recordseries()
         main_event_user = yield User.objects.set_db(self.db).get({"id": main_event.user})
         # test one child field from list
         rs_from_db = yield self.model.objects.set_db(self.db)\
@@ -124,6 +123,40 @@ class TestSerializationReferenceList(BaseSerializationTest):
             main_event, main_event_user=main_event_user,
             prefetched_records='records.event.user',
             prefetched_main_event='event.user', prefetched_simplies=True)
+
+    @gen_test
+    def test_prefetch_related_all_list_child_fields(self):
+        data_list = []
+        for i in range(3):
+            data = yield self._create_recordseries()
+            data_list.append(data)
+        rss_from_db = yield self.model.objects.set_db(self.db)\
+            .prefetch_related('records.event.user').all()
+        for data, rs_from_db in zip(
+                sorted(data_list, key=lambda x: x[0].pk),
+                sorted(rss_from_db, key=lambda x: x.pk)):
+            rs, simplies, records, main_event = data
+        self.assertRecordSeriesEquals(rs_from_db, simplies, records,
+            main_event, prefetched_records='records.event.user')
+
+    @gen_test
+    def test_prefetch_related_filter_list_child_fields(self):
+        data_list = []
+        for i in range(4):
+            data = yield self._create_recordseries()
+            data_list.append(data)
+        ids = [data_list[1][0].pk, data_list[2][0].pk]
+        rss_from_db = yield self.model.objects.set_db(self.db)\
+            .prefetch_related('records.event.user', 'simplies')\
+            .filter({"id": {"$in": ids}}).all()
+        self.assertEqual(len(rss_from_db), 2)
+        for data, rs_from_db in zip(
+                sorted(data_list[1:3], key=lambda x: x[0].pk),
+                sorted(rss_from_db, key=lambda x: x.pk)):
+            rs, simplies, records, main_event = data
+            self.assertRecordSeriesEquals(rs_from_db, simplies, records,
+                main_event, prefetched_records='records.event.user',
+                prefetched_simplies=True)
 
     def assertRecordSeriesEquals(self, rs_from_db, simplies, records,
             main_event, main_event_user=None, prefetched_simplies=False,
@@ -170,7 +203,7 @@ class TestSerializationReferenceList(BaseSerializationTest):
         # json_from_db = record_from_db.to_primitive()
 
     @gen.coroutine
-    def _create_recordseires(self, records_count=3, simplies_count=2, commit=True):
+    def _create_recordseries(self, records_count=3, simplies_count=2, commit=True):
         simplies = []
         records = []
         for i in range(records_count):
